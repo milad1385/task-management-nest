2a6329610d081f91d0a23a0fe8be584bd8f310d4
Milad salamian <miladsalami1385@gmail.com>
2026-06-12 13:32:02 +0330

    load db config from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,17 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './projects/entities/project.entity';
 import { Task } from './tasks/entities/task.entity';
+import 'dotenv/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'task_management',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Project, Task],
       synchronize: true,
     }),
