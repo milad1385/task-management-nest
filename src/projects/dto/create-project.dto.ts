2f3340e6ffbb93267b7f86437ea4f419e8d04ef7
Milad salamian <miladsalami1385@gmail.com>
2026-06-12 13:17:14 +0330

    add validator decorator in create project dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProjectDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength
+} from 'class-validator';
+import { ProjectStatusEnum } from '../enums/projectStatusEnums';
+
+export class CreateProjectDto {
+  @IsString({ message: 'عنوان باید از نوع رشته باشد' })
+  @IsNotEmpty({ message: 'وارد کردن عنوان اجباری است' })
+  @MinLength(2, { message: 'حداقل 2 کاراکتر برای عنوان ضروری است' })
+  @MaxLength(100, { message: 'حداکثر کاراکتر 100 عدد است' })
+  title: string;
+
+  @IsString({ message: 'توضیحات باید از نوع رشته باشد' })
+  @IsOptional()
+  @MinLength(2, { message: 'حداقل 2 کاراکتر برای توضیحات ضروری است' })
+  @MaxLength(1000, { message: 'حداکثر کاراکتر برای توضیحات 1000 عدد است' })
+  description: string;
+
+  @IsEnum(ProjectStatusEnum, {
+    message: 'وضعیت باید یا disable باشد یا enable',
+  })
+  @IsOptional()
+  status: ProjectStatusEnum;
+}
