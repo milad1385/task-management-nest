ec13a44ae9bbe888086522b282b5172c9f21a341
Milad salamian <miladsalami1385@gmail.com>
2026-06-12 18:12:39 +0330

    add get project dto

---
+++ src/projects/dto/get-projects.dto.ts
@@ -0,0 +1,21 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNumber, IsOptional, IsPositive, Max, Min } from 'class-validator';
+
+export class GetProjectQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'شماره صفحه باید عدد باشد' })
+  @IsPositive({ message: 'شماره صفحه باید بزرگتر از 0 باشد' })
+  @IsInt({ message: 'شماره صفحه باید عدد صحیح باشد' })
+  @Min(1, { message: 'حداقل شماره صفحه عدد 1 است' })
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'تعداد آیتم در هر صفحه باید عدد باشد' })
+  @IsPositive({ message: 'تعداد آیتم باید بزرگتر از 0 باشد' })
+  @IsInt({ message: 'تعداد آیتم باید عدد صحیح باشد' })
+  @Min(1, { message: 'حداقل تعداد آیتم در هر صفحه 1 است' })
+  @Max(100, { message: 'حداکثر تعداد آیتم در هر صفحه 100 است' })
+  limit: number = 10;
+}
