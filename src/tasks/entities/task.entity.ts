761f82207db1939b95b8904f4d0c539584819b53
Milad salamian <miladsalami1385@gmail.com>
2026-06-12 11:56:09 +0330

    add task entity

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,21 @@
+import { Project } from 'src/projects/entities/project.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatusEnum } from '../enums/taskStatusEnums';
+
+@Entity({ name: 'tasks' })
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+  @Column({
+    type: 'enum',
+    enum: TaskStatusEnum,
+    default: TaskStatusEnum.DOING,
+  })
+  status: TaskStatusEnum;
+  @ManyToOne(() => Project, (project) => project.tasks)
+  project: Project;
+}
