31577a700410872d2ef7ba059e775d267fda2dd6
Milad salamian <miladsalami1385@gmail.com>
2026-06-12 21:05:47 +0330

    add UpdateProjectStatusDto

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
+import { IsEnum } from 'class-validator';
+import { ProjectStatusEnum } from '../enums/projectStatusEnums';
 
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectStatusDto {
+  @IsEnum(ProjectStatusEnum, {
+    message: 'وضعیت باید یا disable باشد یا enable',
+  })
+  status: ProjectStatusEnum;
+}
