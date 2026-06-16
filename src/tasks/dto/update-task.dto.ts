776062f269ca39d1011e354ff54148217904109d
Milad salamian <miladsalami1385@gmail.com>
2026-06-17 01:07:31 +0330

    add UpdateTaskStatusDto

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { TaskStatusEnum } from '../enums/taskStatusEnums';
+import { IsEnum } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+
+export class UpdateTaskStatusDto {
+  @IsEnum(TaskStatusEnum, {
+    message: 'وضعیت باید done , doing , cancel باشد',
+  })
+  status: TaskStatusEnum;
+}
